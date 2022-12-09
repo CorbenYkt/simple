e575f089b1bf70451c8c915443bed12fcba95e01
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2022-12-09 19:18:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './pages/Layout';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path='' element={<Layout />}>
+            <Route index path='fozzy' element={<Home />}></Route>
+            <Route path='about' element={<About />}></Route>
+            <Route path='profile' element={<Profile />}></Route>
+            <Route path='*' element={<NotFound />} ></Route>
+          </Route>
+        </Routes>
+      </div>
+
+    </>
   );
 }
 
